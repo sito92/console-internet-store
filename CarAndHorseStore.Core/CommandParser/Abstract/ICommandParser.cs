ce80cf0a779b6e73f9b0e69c1d180d0b2ba0fbea
Artur Stelmach <arturstelmach92@gmail.com>
2015-03-12 09:57:11 +0100

    core architecture

---
+++ CarAndHorseStore.Core/CommandParser/Abstract/ICommandParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarAndHorseStore.Core.CommandParser.Abstract
+{
+    public interface ICommandParser
+    {
+
+    }
+}
--- CarAndHorseStore.Core/System/StoreSystem.cs
+++ CarAndHorseStore.Core/System/StoreSystem.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CarAndHorseStore.Core.CommandParser.Abstract;
+using CarAndHorseStore;
+using CarAndHorseStore.Interfaces;
 
 namespace CarAndHorseStore.Core.System
 {
-    public class StoreSystem
+    public class StoreSystem : IStoreSystem
     {
+        private ICommandParser commandParser;
+        public void StartSystem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopSystem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInUser(IUser user)
+        {
+            //dodaj user do zalogowanych
+        }
+        
     }
 }
